4e934d12ba57ce451c833d3d95d75aafb90d1c8e
adou <adou@lititi.com>
2026-03-09 10:18:01 +0800

    fix: correct testPathIgnorePatterns pattern for worker tests

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ const testPathIgnorePatterns = [
 ];
 
 if (process.env.CI) {
-  testPathIgnorePatterns.push('/worker\\.test\\.js$');
+  testPathIgnorePatterns.push('worker\\.test\\.js');
 }
 
 module.exports = {
